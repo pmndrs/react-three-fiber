230ea13022cf6747ea9bf2c0d6b352a62dbc7b59
Paul Henschel <drcmda@gmail.com>
2019-04-01 12:18:48 +0200

    fix examples

--- examples/components/MeshLine.js
+++ examples/components/MeshLine.js
@@ -14,31 +14,34 @@ function Fatline() {
   const [color] = useState(() => colors[parseInt(colors.length * Math.random())])
   const [ratio] = useState(() => 0.5 + 0.5 * Math.random())
   const [width] = useState(() => Math.max(0.1, 0.3 * Math.random()))
+  // Calculate wiggly curve
   const [curve] = useState(() => {
-    let points = []
     let pos = new THREE.Vector3(30 - 60 * Math.random(), -5, 10 - 20 * Math.random())
-    for (let i = 0; i < 30; i++) {
-      pos.add(new THREE.Vector3(2 - Math.random() * 4, 4 - Math.random() * 2, 5 - Math.random() * 10))
-      points.push(pos.clone())
-    }
-    return new THREE.CatmullRomCurve3(points).getPoints(500)
+    return new Array(30)
+      .fill()
+      .map(() =>
+        pos.add(new THREE.Vector3(2 - Math.random() * 4, 4 - Math.random() * 2, 5 - Math.random() * 10)).clone()
+      )
   })
-
+  // Hook into the render loop and decrease the materials dash-offset
   useRender(() => (material.current.uniforms.dashOffset.value -= 0.0005))
   return (
     <mesh>
+      {/** MeshLine and CMRCurve are a OOP factories, not scene objects, hence all the imperative code in here :-( */}
       <meshLine onUpdate={self => (self.parent.geometry = self.geometry)}>
-        <geometry vertices={curve} onUpdate={self => self.parent.setGeometry(self)} />
+        <geometry onUpdate={self => self.parent.setGeometry(self)}>
+          <catmullRomCurve3 args={[curve]} onUpdate={self => (self.parent.vertices = self.getPoints(500))} />
+        </geometry>
       </meshLine>
+      {/** MeshLineMaterial on the other hand is a regular material, so we can just attach it */}
       <meshLineMaterial
-        ref={material}
         attach="material"
+        ref={material}
         transparent
         depthTest={false}
         lineWidth={width}
-        dashArray={0.1}
         color={color}
-        dashOffset={0.0}
+        dashArray={0.1}
         dashRatio={ratio}
       />
     </mesh>
@@ -48,6 +51,7 @@ function Fatline() {
 function Scene() {
   let group = useRef()
   let theta = 0
+  // Hook into the render loop and rotate the scene a bit
   useRender(() => group.current.rotation.set(0, 5 * Math.sin(THREE.Math.degToRad((theta += 0.02))), 0))
   return (
     <group ref={group}>
--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/Selection'
+import App from './components/MeshLine'
 import './styles.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
