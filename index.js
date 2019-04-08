f36897b4afab8dbcedb9ef1cb5fedbebb827a3f0
dynosmurf <dynosmurf@gmail.com>
2019-04-07 18:02:01 -0700

    Correct export name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Canvas } from './src/canvas'
-import { useRender, useThree, useResrouce } from './src/hooks'
+import { useRender, useThree, useResource } from './src/hooks'
 import { addEffect, invalidate, render, unmountComponentAtNode, apply, applyProps } from './src/reconciler'
 
-export { Canvas, addEffect, invalidate, render, unmountComponentAtNode, apply, applyProps, useRender, useThree, useResrouce }
+export { Canvas, addEffect, invalidate, render, unmountComponentAtNode, apply, applyProps, useRender, useThree, useResource }
