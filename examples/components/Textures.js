1b3f5c70ed507dc3f6d4095b43a1f30573d0c39c
Paul Henschel <drcmda@gmail.com>
2019-10-05 00:19:18 +0200

    add textures example

--- examples/components/Textures.js
+++ examples/components/Textures.js
@@ -1,44 +1,96 @@
 import * as THREE from 'three'
-import ReactDOM from 'react-dom'
-import React, { Suspense } from 'react'
-import { Canvas, useLoader } from 'react-three-fiber'
+import React, { Suspense, useRef, useMemo } from 'react'
+import { Canvas, useLoader, useFrame } from 'react-three-fiber'
 import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib'
 
 RectAreaLightUniformsLib.init()
 
-const url = 'https://raw.githubusercontent.com/flowers1225/threejs-earth/master/src/img/earth4.jpg'
-const bumpUrl = 'https://raw.githubusercontent.com/flowers1225/threejs-earth/master/src/img/earth_bump.jpg'
-const specUrl = 'https://raw.githubusercontent.com/flowers1225/threejs-earth/master/src/img/earth_spec.jpg'
+const makeUrl = file => `https://raw.githubusercontent.com/flowers1225/threejs-earth/master/src/img/${file}.jpg`
 
-function Sphere() {
-  const [texture, bump, spec] = useLoader(THREE.TextureLoader, [url, bumpUrl, specUrl])
+function Earth() {
+  const ref = useRef()
+  const [texture, bump, moon] = useLoader(THREE.TextureLoader, [
+    makeUrl('earth4'),
+    makeUrl('earth_bump'),
+    'http://jaanga.github.io/moon/heightmaps/WAC_GLD100_E000N1800_004P-1024x512.png',
+  ])
+  useFrame(
+    ({ clock }) =>
+      (ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z =
+        Math.cos(clock.getElapsedTime() / 8) * Math.PI)
+  )
   return (
-    <mesh>
-      <sphereBufferGeometry attach="geometry" args={[2, 32, 32]} />
-      <meshPhysicalMaterial attach="material" map={texture} bumpMap={bump} bumpScale={0.3} metalnessMap={spec} />
-    </mesh>
+    <group ref={ref}>
+      <Stars />
+      <rectAreaLight
+        intensity={1}
+        position={[10, 10, 10]}
+        width={10}
+        height={1000}
+        onUpdate={self => self.lookAt(new THREE.Vector3(0, 0, 0))}
+      />
+      <rectAreaLight
+        intensity={1}
+        position={[-10, -10, -10]}
+        width={1000}
+        height={10}
+        onUpdate={self => self.lookAt(new THREE.Vector3(0, 0, 0))}
+      />
+      <mesh>
+        <sphereBufferGeometry attach="geometry" args={[2, 64, 64]} />
+        <meshStandardMaterial attach="material" map={texture} bumpMap={bump} bumpScale={0.05} />
+      </mesh>
+      <mesh position={[5, 0, -10]}>
+        <sphereBufferGeometry attach="geometry" args={[0.5, 64, 64]} />
+        <meshStandardMaterial attach="material" color="gray" map={moon} />
+      </mesh>
+    </group>
+  )
+}
+
+function Stars({ count = 5000 }) {
+  const positions = useMemo(() => {
+    let positions = []
+    for (let i = 0; i < count; i++) {
+      positions.push(Math.random() * 10 * (Math.round(Math.random()) ? -40 : 40))
+      positions.push(Math.random() * 10 * (Math.round(Math.random()) ? -40 : 40))
+      positions.push(Math.random() * 10 * (Math.round(Math.random()) ? -40 : 40))
+    }
+    return new Float32Array(positions)
+  }, [count])
+  return (
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          attachObject={['attributes', 'position']}
+          count={positions.length / 3}
+          array={positions}
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <pointsMaterial attach="material" size={1} sizeAttenuation color="white" transparent opacity={0.8} />
+    </points>
   )
 }
 
 export default () => (
-  <Canvas>
-    <ambientLight intensity={0.1} />
+  <Canvas
+    style={{ background: 'radial-gradient(at 50% 70%, #200f20 40%, #090b1f 80%, #050523 100%)' }}
+    camera={{ position: [0, 0, 10], fov: 40 }}
+    onCreated={({ gl }) => {
+      gl.gammaInput = true
+      gl.toneMapping = THREE.ACESFilmicToneMapping
+    }}>
+    <pointLight intensity={0.1} position={[10, 10, 10]} />
     <rectAreaLight
       intensity={3}
-      position={[10, 10, 10]}
-      width={10}
-      height={1000}
-      onUpdate={self => self.lookAt(new THREE.Vector3(0, 0, 0))}
-    />
-    <rectAreaLight
-      intensity={2}
-      position={[-10, -10, -10]}
-      width={1000}
-      height={10}
+      position={[0, 10, -10]}
+      width={30}
+      height={30}
       onUpdate={self => self.lookAt(new THREE.Vector3(0, 0, 0))}
     />
     <Suspense fallback={null}>
-      <Sphere />
+      <Earth />
     </Suspense>
   </Canvas>
 )
