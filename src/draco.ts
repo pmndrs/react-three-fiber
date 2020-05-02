c66243bcae659196bd1304b55e4b7523a2ede0e2
Paul Henschel <drcmda@gmail.com>
2020-05-02 14:47:44 +0200

    fix draco

--- src/draco.ts
+++ src/draco.ts
@@ -1,10 +1,11 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { Loader } from 'three'
 
 export function draco(url: string = '/draco-gltf/') {
-  return (loader: GLTFLoader) => {
+  return (loader: Loader) => {
     const dracoLoader = new DRACOLoader()
     dracoLoader.setDecoderPath(url)
-    loader.setDRACOLoader(dracoLoader)
+    ;(loader as GLTFLoader).setDRACOLoader(dracoLoader)
   }
 }
