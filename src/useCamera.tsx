694c1815abd39423c18fbd7c55c7465158ecd5a3
Paul Henschel <drcmda@gmail.com>
2020-05-18 14:17:20 +0200

    allow refs in useCam

--- src/useCamera.tsx
+++ src/useCamera.tsx
@@ -2,13 +2,13 @@ import { Raycaster, Camera, Intersection } from 'three'
 import React, { useState } from 'react'
 import { useThree, applyProps } from 'react-three-fiber'
 
-export function useCamera(camera: Camera, props?: Partial<Raycaster>) {
+export function useCamera(camera: Camera | React.MutableRefObject<Camera>, props?: Partial<Raycaster>) {
   const { mouse } = useThree()
   const [raycast] = useState(() => {
     let raycaster = new Raycaster()
     if (props) applyProps(raycaster, props, {})
     return function (_: Raycaster, intersects: Intersection[]): void {
-      raycaster.setFromCamera(mouse, camera)
+      raycaster.setFromCamera(mouse, camera instanceof Camera ? camera : camera.current)
       const rc = this.constructor.prototype.raycast.bind(this)
       if (rc) rc(raycaster, intersects)
     }
