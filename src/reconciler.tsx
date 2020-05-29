cbdc52e8811ec3091f734cff36cb529653323fe2
reak998 <66107266+reak998@users.noreply.github.com>
2020-05-29 13:02:53 +0200

    Fix portal crash. (#435)

--- src/reconciler.tsx
+++ src/reconciler.tsx
@@ -318,7 +318,8 @@ function removeChild(parentInstance: any, child: any) {
       parentInstance.remove(child)
     } else {
       child.parent = null
-      parentInstance.__objects = parentInstance.__objects.filter((x: any) => x !== child)
+      if (parentInstance.__objects)
+        parentInstance.__objects = parentInstance.__objects.filter((x: any) => x !== child)
       // Remove attachment
       if (child.attachArray)
         parentInstance[child.attachArray] = parentInstance[child.attachArray].filter((x: any) => x !== child)
