27cdf89374aabfd24b80c3cfe994d59015983340
Michael Fasani <michael.fasani@squirro.com>
2020-06-02 23:52:40 +0200

    Clean up old ideas, unused code

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { HashRouter as Router } from 'react-router-dom'
 import Intro from './pages/Intro'
-import Why from './pages/Why'
 import { Global } from './styles'
 
 export default function App() {
