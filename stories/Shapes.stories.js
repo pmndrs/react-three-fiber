c75b48e81aec6d78b71ddb592fade85756c8d508
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-06-05 21:38:12 +0200

    ignores prettier for polyhedron arguments

--- stories/Shapes.stories.js
+++ stories/Shapes.stories.js
@@ -41,63 +41,38 @@ function Story({ comp, args }) {
   )
 }
 
-export const Box = () => <Story comp='Box' />
-export const Circle = () => <Story comp='Circle' />
-export const Cone = () => <Story comp='Cone' />
-export const Cylinder = () => <Story comp='Cylinder' />
-export const Sphere = () => <Story comp='Sphere' />
-export const Plane = () => <Story comp='Plane' />
-export const Tube = () => <Story comp='Tube' />
-export const Torus = () => <Story comp='Torus' />
-export const TorusKnot = () => <Story comp='TorusKnot' />
-export const Tetrahedron = () => <Story comp='Tetrahedron' />
-export const Ring = () => <Story comp='Ring' />
+export const Box = () => <Story comp="Box" />
+export const Circle = () => <Story comp="Circle" />
+export const Cone = () => <Story comp="Cone" />
+export const Cylinder = () => <Story comp="Cylinder" />
+export const Sphere = () => <Story comp="Sphere" />
+export const Plane = () => <Story comp="Plane" />
+export const Tube = () => <Story comp="Tube" />
+export const Torus = () => <Story comp="Torus" />
+export const TorusKnot = () => <Story comp="TorusKnot" />
+export const Tetrahedron = () => <Story comp="Tetrahedron" />
+export const Ring = () => <Story comp="Ring" />
 
-var verticesOfCube = [-1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1, -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1]
+// prettier-ignore
+const verticesOfCube = [
+  -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1,
+  -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1,
+];
 
-var indicesOfFaces = [
-  2,
-  1,
-  0,
-  0,
-  3,
-  2,
-  0,
-  4,
-  7,
-  7,
-  3,
-  0,
-  0,
-  1,
-  5,
-  5,
-  4,
-  0,
-  1,
-  2,
-  6,
-  6,
-  5,
-  1,
-  2,
-  3,
-  7,
-  7,
-  6,
-  2,
-  4,
-  5,
-  6,
-  6,
-  7,
-  4,
-]
+// prettier-ignore
+const indicesOfFaces = [
+  2, 1, 0, 0, 3, 2,
+  0, 4, 7, 7, 3, 0,
+  0, 1, 5, 5, 4, 0,
+  1, 2, 6, 6, 5, 1,
+  2, 3, 7, 7, 6, 2,
+  4, 5, 6, 6, 7, 4
+];
 
-export const Polyhedron = () => <Story comp='Polyhedron' args={[verticesOfCube, indicesOfFaces]} />
-export const Icosahedron = () => <Story comp='Icosahedron' />
-export const Octahedron = () => <Story comp='Octahedron' />
-export const Dodecahedron = () => <Story comp='Dodecahedron' />
-export const Extrude = () => <Story comp='Extrude' />
-export const Lathe = () => <Story comp='Lathe' />
-export const Parametric = () => <Story comp='Parametric' />
+export const Polyhedron = () => <Story comp="Polyhedron" args={[verticesOfCube, indicesOfFaces]} />
+export const Icosahedron = () => <Story comp="Icosahedron" />
+export const Octahedron = () => <Story comp="Octahedron" />
+export const Dodecahedron = () => <Story comp="Dodecahedron" />
+export const Extrude = () => <Story comp="Extrude" />
+export const Lathe = () => <Story comp="Lathe" />
+export const Parametric = () => <Story comp="Parametric" />
