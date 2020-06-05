546b4f3be61faf098ab0518fa8eb1720933473ab
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-06-05 22:35:49 +0200

    Adds theming

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+import dreiLogo from './drei.jpeg'
+
+export default create({
+    base: "light",
+    brandImage: dreiLogo,
+    appBg: "white"
+})
