4d334d265f1104c366740ef2787b4823cc826693
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-06-06 11:59:07 +0200

    Adds story source addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/preset-typescript', '@storybook/addon-actions', '@storybook/addon-links'],
+  addons: [
+    '@storybook/preset-typescript',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource'
+  ],
 }
