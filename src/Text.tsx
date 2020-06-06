43c7b6b5c6655c8320cf45bad3d764d7e6c83f41
Paul Henschel <drcmda@gmail.com>
2020-06-06 15:15:03 +0200

    fix text default color

--- src/Text.tsx
+++ src/Text.tsx
@@ -35,42 +35,32 @@ type Props = JSX.IntrinsicElements['mesh'] & {
   whiteSpace?: 'normal' | 'overflowWrap' | 'overflowWrap'
 }
 
-export const Text = forwardRef(
-  ({ color = 'black', anchorX = 'center', anchorY = 'middle', children, ...props }: Props, ref) => {
-    const textRef = useRef<TextMeshImpl>()
-    const [baseMtl, setBaseMtl] = useState()
-    const [nodes, text] = useMemo(() => {
-      let n: React.ReactNode[] = []
-      let t = ''
-      Children.forEach(children, (child) => {
-        if (typeof child === 'string') t += child
-        else if (child && typeof child === 'object' && (child as React.ReactElement).props.attach === 'material') {
-          // Instantiate the base material and grab a reference to it, but don't assign any
-          // props, and assign it as the `material`, which Troika will replace behind the scenes.
-          n.push(createElement((child as React.ReactElement).type, { ref: setBaseMtl, attach: 'material' }))
-          // Once the base material has been assigned, grab the resulting upgraded material,
-          // and apply the original material props to that.
-          if (baseMtl) {
-            n.push(
-              <primitive object={textRef.current.material} {...(child as React.ReactElement).props} attach={null} />
-            )
-          }
-        } else n.push(child)
-      })
-      return [n, t]
-    }, [children, baseMtl])
-    useLayoutEffect(() => void textRef.current.sync())
+export const Text = forwardRef(({ anchorX = 'center', anchorY = 'middle', children, ...props }: Props, ref) => {
+  const textRef = useRef<TextMeshImpl>()
+  const [baseMtl, setBaseMtl] = useState()
+  const [nodes, text] = useMemo(() => {
+    let n: React.ReactNode[] = []
+    let t = ''
+    Children.forEach(children, (child) => {
+      if (typeof child === 'string') t += child
+      else if (child && typeof child === 'object' && (child as React.ReactElement).props.attach === 'material') {
+        // Instantiate the base material and grab a reference to it, but don't assign any
+        // props, and assign it as the `material`, which Troika will replace behind the scenes.
+        n.push(createElement((child as React.ReactElement).type, { ref: setBaseMtl, attach: 'material' }))
+        // Once the base material has been assigned, grab the resulting upgraded material,
+        // and apply the original material props to that.
+        if (baseMtl) {
+          n.push(<primitive object={textRef.current.material} {...(child as React.ReactElement).props} attach={null} />)
+        }
+      } else n.push(child)
+    })
+    return [n, t]
+  }, [children, baseMtl])
+  useLayoutEffect(() => void textRef.current.sync())
 
-    return (
-      <textMeshImpl
-        ref={mergeRefs([textRef, ref])}
-        text={text}
-        color={color}
-        anchorX={anchorX}
-        anchorY={anchorY}
-        {...props}>
-        {nodes}
-      </textMeshImpl>
-    )
-  }
-)
+  return (
+    <textMeshImpl ref={mergeRefs([textRef, ref])} text={text} anchorX={anchorX} anchorY={anchorY} {...props}>
+      {nodes}
+    </textMeshImpl>
+  )
+})
