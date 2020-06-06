9e27ce249ecda77753954dc09bb871c5ea32c072
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-06-06 11:31:04 +0200

    Adds controls by default to setup scene

--- .storybook/Setup.tsx
+++ .storybook/Setup.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
 
-export function Setup({ children, cameraPosition = [-5, 5, 5] }) {
+import { OrbitControls } from '../src/OrbitControls'
+
+export function Setup({ children, cameraPosition = [-5, 5, 5], controls = true }) {
     return (
         <Canvas colorManagement shadowMap camera={{ position: cameraPosition }} pixelRatio={window.devicePixelRatio}>
             {children}
             <ambientLight intensity={0.8} />
             <pointLight intensity={1} position={[0, 6, 0]} />
+            {controls && <OrbitControls />}
         </Canvas>
     )
 }
