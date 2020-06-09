d5ba86d20c83af90f67064f83bc0d47646fbe811
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-06-09 12:27:55 +0200

    Adds MeshWobbleMaterial story

---
+++ .storybook/stories/MeshWobbleMaterial.stories.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { Setup } from '../Setup'
+
+import { MeshWobbleMaterial } from '../../src/MeshWobbleMaterial'
+import { Torus } from '../../src/shapes'
+import { useTurntable } from '../useTurntable'
+
+export default {
+    title: 'Shaders.MeshWobbleMaterial',
+    component: MeshWobbleMaterial,
+    decorators: [(storyFn) => <Setup> {storyFn()}</Setup>],
+}
+
+function MeshWobbleMaterialScene() {
+
+    return (
+        <Torus args={[1, 0.25, 16, 100]}>
+            <MeshWobbleMaterial attach="material" color="#f25042" speed={1} factor={1} />
+        </Torus>
+    )
+}
+
+export const MeshWobbleMaterialSt = () => <MeshWobbleMaterialScene />
+MeshWobbleMaterialSt.story = {
+    name: 'Default',
+}
