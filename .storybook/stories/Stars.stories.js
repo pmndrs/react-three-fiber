f30946b87c5a22a41080357e96e53a042f1add8d
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-06-12 16:23:01 +0200

    adds Stars story

---
+++ .storybook/stories/Stars.stories.js
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react'
+
+import { Setup } from '../Setup'
+
+import { Stars } from '../../src/Stars'
+import { Plane } from '../../src/shapes'
+
+export default {
+  title: 'Abstractions.Stars',
+  component: Stars,
+  decorators: [(storyFn) => <Setup> {storyFn()}</Setup>],
+}
+
+function StarsScene() {
+  return (
+    <>
+      <Stars />
+      <Plane rotation-x={Math.PI / 2} args={[100, 100, 4, 4]}>
+        <meshBasicMaterial color="black" wireframe attach="material" />
+      </Plane>
+      <axesHelper />
+    </>
+  )
+}
+
+export const StarsSt = () => <StarsScene />
+StarsSt.story = {
+  name: 'Default',
+}
