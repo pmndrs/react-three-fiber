ce1f8a1e7d0c84e10fc4f5839ee82d48bca04a29
Paul Henschel <drcmda@gmail.com>
2020-06-13 11:43:31 +0200

    fix scroll demo

--- examples/config-overrides.js
+++ examples/config-overrides.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = (config, env) => {
   config.resolve.extensions = [...config.resolve.extensions, '.ts', '.tsx']
   return override(
-    addReactRefresh({ disableRefreshCheck: true }),
+    addReactRefresh(),
     removeModuleScopePlugin(),
     babelInclude([path.resolve('src'), path.resolve('../src')]),
     addWebpackAlias({
