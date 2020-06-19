1d598ec3eb24c94f47ed673a5bd8c126b38e8806
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-06-19 11:54:33 +0200

    adds useAspect story

---
+++ .storybook/stories/useAspect.stories.js
@@ -0,0 +1,54 @@
+import React, { Suspense } from 'react'
+import { useLoader } from 'react-three-fiber'
+import { useAspect } from '../../src/useAspect'
+
+import { Plane } from '../../src/shapes'
+
+import { Setup } from '../Setup'
+import { TextureLoader } from 'three'
+
+export default {
+  title: 'Misc.useAspect',
+  component: useAspect,
+  decorators: [(storyFn) => <Setup cameraPosition={[0, -10, 0]}>{storyFn()}</Setup>],
+}
+
+function Simple(props) {
+  const scale = useAspect('cover', 1920, 1080, 1)
+
+  return (
+    <Plane scale={scale} rotation-x={Math.PI / 2} args={[1, 1, 4, 4]}>
+      <meshPhongMaterial attach="material" wireframe />
+    </Plane>
+  )
+}
+
+export const DefaultStory = () => (
+  <Suspense fallback="">
+    <Simple />
+  </Suspense>
+)
+DefaultStory.story = {
+  name: 'Default',
+}
+
+function WithTexture(props) {
+  const scale = useAspect('cover', 1920, 1080, 1)
+
+  const [map] = useLoader(TextureLoader, [`https://source.unsplash.com/random/1920x1080`])
+
+  return (
+    <Plane scale={scale} rotation-x={Math.PI / 2}>
+      <meshPhongMaterial attach="material" map={map} color="grey" />
+    </Plane>
+  )
+}
+
+export const TextureStory = () => (
+  <Suspense fallback="">
+    <WithTexture />
+  </Suspense>
+)
+TextureStory.story = {
+  name: 'With Texture',
+}
