0e3a962c17275122ecc12865db9763b29db44c88
emmelleppi <mlp.perego@gmail.com>
2020-07-01 17:45:46 +0200

    Adds MapControls story

---
+++ .storybook/stories/MapControls.stories.js
@@ -0,0 +1,67 @@
+import React, { Suspense, useState, useRef, useEffect, useMemo } from 'react'
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader'
+import * as THREE from 'three'
+import { useLoader, Canvas } from 'react-three-fiber'
+
+import { MapControls } from '../../src/MapControls'
+import mapSvg from '../map.svg'
+
+export default {
+    title: 'Controls.MapControls',
+    component: MapControlsScene,
+}
+
+function Cell(props) {
+    const { color, shape, fillOpacity } = props
+
+    return (
+        <mesh>
+            <meshBasicMaterial attach="material" color={color} opacity={fillOpacity} depthWrite={false} transparent />
+            <shapeBufferGeometry attach="geometry" args={[shape]} />
+        </mesh>
+    )
+}
+
+function Svg() {
+    const [center, setCenter] = useState([0, 0, 0])
+    const ref = useRef()
+
+    const { paths } = useLoader(SVGLoader, mapSvg)
+    
+    const shapes = useMemo(
+        () => paths.flatMap(p => p.toShapes(true).map(shape => ({ shape, color: p.color, fillOpacity: p.userData.style.fillOpacity }))),
+        [paths]
+    )
+
+    useEffect(() => {
+        const box = new THREE.Box3().setFromObject(ref.current)
+        const sphere = new THREE.Sphere()
+        box.getBoundingSphere(sphere)
+        setCenter([-sphere.center.x, -sphere.center.y, 0])
+    }, [])
+
+    return (
+        <group position={center} ref={ref}>
+        {shapes.map(props => (
+            <Cell key={props.shape.uuid} {...props} />
+        ))}
+        </group>
+    )
+}
+
+function MapControlsScene() {
+    return (
+        <Canvas orthographic camera={{ position: [0, 0, 50], zoom: 10, up: [0, 0, 1], far: 10000 }}>
+            <color attach="background" args={[0xf3f3f3]} />
+            <Suspense fallback={null}>
+                <Svg />
+            </Suspense>
+            <MapControls />
+        </Canvas>
+    )
+}
+
+export const MapControlsSceneSt = () => <MapControlsScene />
+MapControlsSceneSt.story = {
+    name: 'Default',
+}
