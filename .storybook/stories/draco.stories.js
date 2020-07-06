17a2cdc465aec0d91bced27568681e283ca95f5a
emmelleppi <mlp.perego@gmail.com>
2020-07-06 13:22:00 +0200

    Adds draco story

---
+++ .storybook/stories/draco.stories.js
@@ -0,0 +1,36 @@
+import React, { Suspense } from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useLoader } from 'react-three-fiber'
+
+import { Setup } from '../Setup'
+import { draco } from '../../src/draco'
+
+export default {
+    title: 'Misc.draco',
+    component: DracoScene,
+    decorators: [(storyFn) => <Setup cameraPosition={[0, 0, 5]}>{storyFn()}</Setup>],
+}
+
+function Suzanne() {
+  const { nodes, materials } = useLoader(GLTFLoader, "../suzanne.glb", draco("../draco-gltf/"))
+
+  return (
+    <group dispose={null}>
+      <mesh material={materials['Material.001']} geometry={nodes.Suzanne.geometry} />
+    </group>
+  )
+}
+
+function DracoScene() {
+
+      return (
+        <Suspense fallback={null}>
+          <Suzanne />
+        </Suspense>
+      )
+}
+
+export const DracoSceneSt = () => <DracoScene />
+DracoSceneSt.story = {
+    name: 'Default',
+}
