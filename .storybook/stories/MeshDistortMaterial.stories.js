a7470c232baa86bb80b1f4988a4b0f93b4160f02
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-08-01 11:07:19 +0200

    adds distort material knobs

--- .storybook/stories/MeshDistortMaterial.stories.js
+++ .storybook/stories/MeshDistortMaterial.stories.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { withKnobs, number, boolean } from '@storybook/addon-knobs'
+
 import { Setup } from '../Setup'
 import { MeshDistortMaterial } from '../../src/MeshDistortMaterial'
 import { Icosahedron } from '../../src/shapes'
@@ -7,15 +9,18 @@ import { Icosahedron } from '../../src/shapes'
 export default {
   title: 'Shaders/MeshDistortMaterial',
   component: MeshDistortMaterial,
-  decorators: [(storyFn) => <Setup> {storyFn()}</Setup>],
+  decorators: [withKnobs, (storyFn) => <Setup> {storyFn()}</Setup>],
 }
-
-console.log(MeshDistortMaterial)
 
 function MeshDistortMaterialScene() {
   return (
     <Icosahedron args={[1, 4]}>
-      <MeshDistortMaterial attach="material" color="#f25042" distort={0.4} radius={1} />
+      <MeshDistortMaterial
+        attach="material"
+        color="#f25042"
+        distort={number('Distort', 0.4, { range: true, min: 0, max: 1, step: 0.1 })}
+        radius={number('Radius', 1, { range: true, min: 0, max: 1, step: 0.1 })}
+      />
     </Icosahedron>
   )
 }
