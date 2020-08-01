ec3783de68248c3a9d9c0f40b0e484be2f8166c2
Giulio Zausa <giulio.zausa@smc.it>
2020-08-01 18:48:45 +0200

    WIP Added fix for WebGL1 context creation

--- src/targets/web.tsx
+++ src/targets/web.tsx
@@ -1,19 +1,20 @@
 export * from '../index'
 export * from '../canvas'
 
-import { WebGLRenderer } from 'three'
+import { WebGLRenderer, WebGL1Renderer } from 'three'
 import React, { useRef } from 'react'
 import { ResizeContainer, ContainerProps } from './shared/web/ResizeContainer'
 
 export const Canvas = React.memo(function Canvas({ children, ...props }: ContainerProps) {
   const canvasRef = useRef<HTMLCanvasElement>()
+  const renderer = WebGL1Renderer ? (props.gl2 ? WebGLRenderer : WebGL1Renderer) : WebGLRenderer
   return (
     <ResizeContainer
       {...props}
       renderer={() => {
         if (canvasRef.current) {
           const params = { antialias: true, alpha: true, ...props.gl }
-          const temp = new WebGLRenderer({
+          const temp = new renderer({
             powerPreference: 'high-performance',
             //stencil: false,
             //depth: false,
