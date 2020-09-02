469f09dcf12d2c09d66e68bef37ac9954a1486ed
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-09-02 18:51:24 +0200

    sky

--- .storybook/stories/Sky.stories.js
+++ .storybook/stories/Sky.stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withKnobs, number } from '@storybook/addon-knobs'
 
 import { Setup } from '../Setup'
 
@@ -8,13 +9,13 @@ import { Plane } from '../../src/shapes'
 export default {
   title: 'Shaders/Sky',
   component: Sky,
-  decorators: [(storyFn) => <Setup> {storyFn()}</Setup>],
+  decorators: [withKnobs, (storyFn) => <Setup> {storyFn()}</Setup>],
 }
 
 function SkyScene() {
   return (
     <>
-      <Sky sunPosition={[0, Math.PI, -1]} />
+      <Sky />
       <Plane rotation-x={Math.PI / 2} args={[100, 100, 4, 4]}>
         <meshBasicMaterial color="black" wireframe attach="material" />
       </Plane>
@@ -25,3 +26,23 @@ function SkyScene() {
 
 export const SkySt = () => <SkyScene />
 SkySt.storyName = 'Default'
+
+function SkyScene2() {
+  return (
+    <>
+      <Sky 
+        distance={3000} 
+        turbidity={number('Turbidity', 8, { range: true, max: 10, step: 0.1 })} 
+        rayleigh={number('Rayleigh', 6, { range: true, max: 10, step: 0.1 })} 
+        sunPosition={[Math.PI, 0, 0]}
+        />
+      <Plane rotation-x={Math.PI / 2} args={[100, 100, 4, 4]}>
+        <meshBasicMaterial color="black" wireframe attach="material" />
+      </Plane>
+      <axesHelper />
+    </>
+  )
+}
+
+export const SkySt2 = () => <SkyScene2 />
+SkySt2.storyName = 'Another one'
--- src/shaders/Sky.tsx
+++ src/shaders/Sky.tsx
@@ -3,11 +3,11 @@ import { ReactThreeFiber, extend } from 'react-three-fiber'
 import { Sky as SkyImpl } from 'three/examples/jsm/objects/Sky'
 import { Vector3 } from 'three'
 
-extend({ SkyImpl })
-
 export type Sky = {
   distance?: number
   sunPosition?: ReactThreeFiber.Vector3
+  turbidity?: number
+  rayleigh?: number
 } & ReactThreeFiber.Object3DNode<SkyImpl, typeof SkyImpl>
 
 declare global {
@@ -18,7 +18,21 @@ declare global {
   }
 }
 
-export const Sky = forwardRef<Sky>(({ distance = 450000, sunPosition = [0, 1, 0], ...props }: Sky, ref) => {
-  const scale = useMemo(() => new Vector3().setScalar(distance), [distance])
-  return <skyImpl ref={ref} material-uniforms-sunPosition-value={sunPosition} scale={scale} {...props} />
-})
+export const Sky = forwardRef<Sky>(
+  ({ distance = 100, turbidity = 2, rayleigh = 1, sunPosition = [0, Math.PI, 0], ...props }: Sky, ref) => {
+    const scale = useMemo(() => new Vector3().setScalar(distance), [distance])
+    const sky = useMemo(() => new SkyImpl(), [])
+
+    return (
+      <primitive
+        object={sky}
+        ref={ref}
+        material-uniforms-turbidity-value={turbidity}
+        material-uniforms-rayleigh-value={rayleigh}
+        material-uniforms-sunPosition-value={sunPosition}
+        scale={scale}
+        {...props}
+      />
+    )
+  }
+)
