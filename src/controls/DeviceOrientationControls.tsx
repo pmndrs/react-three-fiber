ad942b48f12798f82e5159d08d54b6322a951ec4
Paul Henschel <drcmda@gmail.com>
2020-09-02 13:28:21 +0200

    add deviceorientationcontrols

--- src/controls/DeviceOrientationControls.tsx
+++ src/controls/DeviceOrientationControls.tsx
@@ -1,31 +1,20 @@
-import React, { forwardRef, useRef, useEffect } from 'react'
-import { ReactThreeFiber, extend, useThree, useFrame, Overwrite } from 'react-three-fiber'
+import React, { forwardRef, useMemo, useEffect } from 'react'
+import { ReactThreeFiber, useThree, useFrame, Overwrite } from 'react-three-fiber'
 import { DeviceOrientationControls as DeviceOrientationControlsImp } from 'three/examples/jsm/controls/DeviceOrientationControls'
-import mergeRefs from 'react-merge-refs'
-
-extend({ DeviceOrientationControlsImp })
 
 export type DeviceOrientationControls = Overwrite<
   ReactThreeFiber.Object3DNode<DeviceOrientationControlsImp, typeof DeviceOrientationControlsImp>,
   { target?: ReactThreeFiber.Vector3 }
 >
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      deviceOrientationControlsImp: DeviceOrientationControls
-    }
-  }
-}
-
 export const DeviceOrientationControls = forwardRef((props: DeviceOrientationControls, ref) => {
-  const controls = useRef<DeviceOrientationControlsImp>()
   const { camera } = useThree()
-  useFrame(() => controls.current?.update())
+  const controls = useMemo(() => new DeviceOrientationControlsImp(camera), [camera])
+  useFrame(() => controls.enabled && controls.update())
   useEffect(() => {
-    const currentControl = controls.current
-    currentControl?.connect()
-    return () => currentControl?.dispose()
-  })
-  return <deviceOrientationControlsImp ref={mergeRefs([controls, ref])} args={[camera]} {...props} />
+    const current = controls
+    current.connect()
+    return () => current.dispose()
+  }, [])
+  return <primitive object={controls} ref={ref} {...props} />
 })
