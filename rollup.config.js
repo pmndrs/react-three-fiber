f542df83320b6b50a7db64c141606ea7fd82fb08
Paul Henschel <drcmda@gmail.com>
2020-09-07 01:49:48 +0200

    generate individual files + flatbundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import json from 'rollup-plugin-json'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 import glslify from 'rollup-plugin-glslify'
+import multiInput from 'rollup-plugin-multi-input'
 
 const root = process.platform === 'win32' ? path.resolve('/') : '/'
 const external = (id) => !id.startsWith('.') && !id.startsWith(root)
@@ -28,14 +29,40 @@ const getBabelOptions = ({ useESModules }, targets) => ({
 
 export default [
   {
+    input: ['src/**/*.tsx', '!src/*.tsx'],
+    output: { dir: `dist`, format: 'esm' },
+    external,
+    plugins: [
+      multiInput(),
+      json(),
+      glslify(),
+      babel(getBabelOptions({ useESModules: true }, '>1%, not dead, not ie 11, not op_mini all')),
+      resolve({ extensions }),
+    ],
+  },
+  {
     input: `./src/index.tsx`,
-    output: { file: `dist/index.js`, format: 'esm' },
+    output: { dir: `dist`, format: 'esm' },
     external,
     plugins: [
       json(),
       glslify(),
       babel(getBabelOptions({ useESModules: true }, '>1%, not dead, not ie 11, not op_mini all')),
       sizeSnapshot(),
+      resolve({ extensions }),
+    ],
+  },
+  {
+    input: ['src/**/*.tsx', '!src/*.tsx'],
+    output: { dir: `dist`, format: 'cjs' },
+    external,
+    plugins: [
+      multiInput({
+        transformOutputPath: (output) => path.dirname(output) + '/' + path.basename(output, '.tsx') + '.cjs.js',
+      }),
+      json(),
+      glslify(),
+      babel(getBabelOptions({ useESModules: false })),
       resolve({ extensions }),
     ],
   },
