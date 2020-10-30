7797b5415b884233aa6ebf98df53fd770dc9afc1
Paul Henschel <drcmda@gmail.com>
2020-10-30 16:31:41 +0100

    broken test

--- examples/src/demos/dev/Test.js
+++ examples/src/demos/dev/Test.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Canvas, Renderer } from 'react-three-fiber'
 
-console.log(Renderer)
-
-function Box({ color, position }) {
+function Box({ stop = false, color, position }) {
   return (
     <mesh
       name={color}
       position={position}
-      onPointerOver={(e) => e.stopPropagation() || console.log(`Box${color} pointerOver`)}
-      onPointerOut={(e) => e.stopPropagation() || console.log(`Box${color} pointerOut`)}
+      onPointerOver={(e) => {
+        if (stop) e.stopPropagation()
+        console.log(`Box${color} pointerOver`)
+      }}
+      onPointerOut={(e) => {
+        if (stop) e.stopPropagation()
+        console.log(`Box${color} pointerOut`)
+      }}
     >
       <boxBufferGeometry args={[10, 10, 10]} />
       <meshPhysicalMaterial color={color} transparent transmission={0.1} />
@@ -22,7 +26,7 @@ export default function App() {
     <Canvas camera={{ fov: 75, position: [-5, 0, 25] }}>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box color="green" position={[7, 0, -2]} />
+      <Box stop color="green" position={[7, 0, -2]} />
       <Box color="red" position={[-5, 0, 0]} />
     </Canvas>
   )
