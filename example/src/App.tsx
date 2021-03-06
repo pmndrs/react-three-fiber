0d4bcbf24ed8a59ef22dabfb32ab834f09d7906b
Paul Henschel <drcmda@gmail.com>
2021-03-06 13:58:19 +0100

    cleanup example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,22 +2,15 @@ import * as React from 'react'
 import * as THREE from 'three'
 import { useThree, useFrame } from 'react-three-fiber'
 
-const col = new THREE.Color(0xff0000)
-
 export default function App() {
-  const material = React.useRef<THREE.MeshBasicMaterial>()
   const [showCube, setShowCube] = React.useState(false)
   const [color, setColor] = React.useState('pink')
 
   React.useEffect(() => {
-    const interval = setInterval(() => {
-      if (material.current) material.current.color = col.set(material.current.color.r === 1 ? 0x0000ff : 0xff0000)
-      setShowCube(!showCube)
-    }, 1000)
+    const interval = setInterval(() => setShowCube(showCube => !showCube), 1000)
     return () => clearInterval(interval)
-  }, [showCube, setShowCube])
+  }, [])
 
-  const handlePointerDown = () => setColor(color === 'pink' ? 'yellow' : 'pink')
   const size = useThree((state) => state.size)
   console.log(size)
   const group = React.useRef<THREE.Group>()
@@ -28,19 +21,19 @@ export default function App() {
   return (
     <>
       <group ref={group}>
-        <mesh onPointerDown={handlePointerDown}>
-          <sphereGeometry args={[1, 32, 32]} />
-          <meshBasicMaterial ref={material} color={col} />
+        <mesh onClick={() => setColor(color === 'pink' ? 'yellow' : 'pink')}>
+          <sphereGeometry args={[0.5, 32, 32]} />
+          <meshBasicMaterial color={showCube ? 0x0000ff : 0xff0000} />
         </mesh>
         {showCube ? (
           <mesh position={[2, 0, 0]}>
-            <boxBufferGeometry args={[1, 1]} />
-            <meshNormalMaterial />
+            <boxGeometry args={[1, 1]} />
+            <meshNormalMaterial  transparent opacity={0.5}/>
           </mesh>
         ) : (
           <mesh>
-            <icosahedronBufferGeometry args={[1]} />
-            <meshBasicMaterial color="orange" />
+            <icosahedronGeometry args={[1]} />
+            <meshBasicMaterial color="orange" transparent opacity={0.5} />
           </mesh>
         )}
       </group>
