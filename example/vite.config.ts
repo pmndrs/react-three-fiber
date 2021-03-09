16bc526f153521fde2295a2389130852f07bf065
David <david.bismut@gmail.com>
2021-03-09 21:46:55 +0100

    ci: preconstruct fix

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,10 +3,5 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      'react-three-fiber': path.resolve('../packages/fiber/src/web/index.tsx'),
-    },
-  },
   plugins: [reactRefresh()],
 })
