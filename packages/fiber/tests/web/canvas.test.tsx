a99fbdd4bf488b161c8c5df6bcd5de0260b05cbd
Josh Ellis <joshua.ellis18@gmail.com>
2021-03-11 17:03:30 +0000

    move to use testing-library/react

--- packages/fiber/tests/web/canvas.test.tsx
+++ packages/fiber/tests/web/canvas.test.tsx
@@ -1,65 +1,42 @@
 jest.mock('scheduler', () => require('scheduler/unstable_mock'))
 
 import * as React from 'react'
-import { ReactTestRenderer, create, act } from 'react-test-renderer'
-import { createCanvas } from 'react-three-test-renderer/src/createTestCanvas'
+import { render, RenderResult } from '@testing-library/react'
 import { createWebGLContext } from 'react-three-test-renderer/src/createWebGLContext'
 
 import { Canvas, testutil_act as r3fAct } from '../../src/web'
 
+// @ts-ignore
+HTMLCanvasElement.prototype.getContext = function () {
+  return createWebGLContext(this)
+}
+
 describe('web Canvas', () => {
-  const canvas = createCanvas({
-    beforeReturn: (canvas) => {
-      //@ts-ignore
-      canvas.getContext = (type: string) => {
-        if (type === 'webgl' || type === 'webgl2') {
-          return createWebGLContext(canvas)
-        }
-      }
-    },
-  })
-
-  const createNodeMock = () => {
-    return canvas
-  }
-
   it('should correctly mount', async () => {
-    let renderer: ReactTestRenderer | null = null
+    let renderer: RenderResult = null!
 
     await r3fAct(async () => {
-      await act(async () => {
-        renderer = create(
-          <Canvas>
-            <group />
-          </Canvas>,
-          {
-            createNodeMock,
-          },
-        )
-      })
+      renderer = render(
+        <Canvas>
+          <group />
+        </Canvas>,
+      )
     })
 
-    const tree = renderer!.toJSON()
-
-    expect(tree).toMatchSnapshot()
+    expect(renderer.container).toMatchSnapshot()
   })
 
   it('should correctly unmount', async () => {
-    let renderer: ReactTestRenderer | null = null
+    let renderer: RenderResult = null!
 
     await r3fAct(async () => {
-      await act(async () => {
-        renderer = create(
-          <Canvas>
-            <group />
-          </Canvas>,
-          {
-            createNodeMock,
-          },
-        )
-      })
+      renderer = render(
+        <Canvas>
+          <group />
+        </Canvas>,
+      )
     })
 
-    expect(() => renderer!.unmount()).not.toThrow()
+    expect(() => renderer.unmount()).not.toThrow()
   })
 })
