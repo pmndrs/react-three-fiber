e67ccf5c653f88aabbc4926edbd886d97e4c3947
Josh Ellis <joshua.ellis18@gmail.com>
2021-03-11 16:05:43 +0000

    remove coverage (for now)

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/packages/*/dist'],
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80,
-    },
-  },
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/packages/fiber/dist'],
+  // coverageThreshold: {
+  //   global: {
+  //     statements: 80,
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //   },
+  // },
   coverageDirectory: './coverage/',
   collectCoverage: true,
   moduleFileExtensions: ['js', 'ts', 'tsx'],
