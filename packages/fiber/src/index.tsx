dcf76aaca160cd0b598e465b8c5f2b7ff87da442
Josh Ellis <joshua.ellis18@gmail.com>
2021-03-12 09:32:46 +0000

    export types as ReactThreeFiber

--- packages/fiber/src/index.tsx
+++ packages/fiber/src/index.tsx
@@ -3,4 +3,7 @@ export { createRenderer as _createRenderer } from './core/renderer'
 export { createStore as _createStore } from './core/store'
 export { createLoop as _createLoop } from './core/loop'
 
+// for type declarations
+export * as ReactThreeFiber from './three-types'
+
 export * from './web'
