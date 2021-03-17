e8756f115dafb020417b07943a8d5101fbda817e
Josh Ellis <joshua.ellis18@gmail.com>
2021-03-17 15:50:43 +0000

    types

--- packages/fiber/src/index.tsx
+++ packages/fiber/src/index.tsx
@@ -4,7 +4,8 @@ export { createStore as _createStore } from './core/store'
 export { createLoop as _createLoop } from './core/loop'
 
 // for type declarations
-export * as ReactThreeFiber from './three-types'
+import * as ReactThreeFiber from './three-types'
+export { ReactThreeFiber }
 export type { Intersection, Subscription, Dpr, Size, Viewport, Camera, RenderCallback, Performance } from './core/store'
 export type { Events, EventManager } from './core/events'
 
