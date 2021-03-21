941f65a5ba238385681b14ce9b31b017dbe849d9
Josh Ellis <joshua.ellis18@gmail.com>
2021-03-21 12:46:37 +0000

    fix namespace in r3f

--- packages/fiber/tests/web/canvas.test.tsx
+++ packages/fiber/tests/web/canvas.test.tsx
@@ -2,7 +2,7 @@ jest.mock('scheduler', () => require('scheduler/unstable_mock'))
 
 import * as React from 'react'
 import { render, RenderResult } from '@testing-library/react'
-import { createWebGLContext } from 'react-three-test-renderer/src/createWebGLContext'
+import { createWebGLContext } from '@react-three/test-renderer/src/createWebGLContext'
 
 import { Canvas, act } from '../../src/web'
 
