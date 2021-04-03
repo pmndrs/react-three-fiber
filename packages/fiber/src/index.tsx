5ba734610131091877c1f433d35dafb72969cba8
Paul Henschel <drcmda@gmail.com>
2021-04-03 23:19:08 +0200

    fix types

--- packages/fiber/src/index.tsx
+++ packages/fiber/src/index.tsx
@@ -1,4 +1,5 @@
 // for type declarations
+export * from './three-types'
 import * as ReactThreeFiber from './three-types'
 export { ReactThreeFiber }
 export type { Intersection, Subscription, Dpr, Size, Viewport, Camera, RenderCallback, Performance } from './core/store'
