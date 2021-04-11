3a66d5abd618a39ec997323ebf3a8724f6b5237f
Paul Henschel <drcmda@gmail.com>
2021-04-11 10:19:55 +0200

    deprecate react-three-fiber

---
+++ packages/deprecated/readme.md
@@ -0,0 +1,1 @@
+This package has been deprecated, please use [@react-three/fiber](http://npmjs.com/@react-three/fiber) from now on.
---
+++ packages/deprecated/src/index.tsx
@@ -0,0 +1,5 @@
+export * from '@react-three/fiber'
+
+console.warn(
+  'react-three-fiber has been deprecated, please use @react-three/fiber (https://www.npmjs.com/package/@react-three/fiber) from now on',
+)
