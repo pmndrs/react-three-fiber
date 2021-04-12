9b660f1d2047039dcb356691a7cbfbfdc21c7076
Paul Henschel <drcmda@gmail.com>
2021-04-12 08:52:15 +0200

    remove deprecated

--- packages/deprecated/readme.md
+++
@@ -1,1 +0,0 @@
-This package has been deprecated, please use [@react-three/fiber](http://npmjs.com/@react-three/fiber) from now on.
--- packages/deprecated/src/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from '@react-three/fiber'
-
-console.warn(
-  'react-three-fiber has been deprecated, please use @react-three/fiber (https://www.npmjs.com/package/@react-three/fiber) from now on',
-)
