2b98f4dc9a5e30532057da9a8d16c4c507aa87fa
David Lindkvist <david@14islands.com>
2021-06-24 12:30:01 +0200

    feat(build): add dev-https script since real WebXR devices require https

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,14 +1,20 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import mkcert from 'vite-plugin-mkcert'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-      '@react-three/fiber': path.resolve('../packages/fiber/src/index.tsx'),
+export default ({ command, mode }) => {
+  return {
+    resolve: {
+      alias: {
+        react: path.resolve('./node_modules/react'),
+        'react-dom': path.resolve('./node_modules/react-dom'),
+        '@react-three/fiber': path.resolve('../packages/fiber/src/index.tsx'),
+      },
     },
-  },
-  plugins: [reactRefresh()],
-})
+    server: {
+      https: command === 'dev-https',
+    },
+    plugins: [reactRefresh(), mkcert()],
+  }
+}
