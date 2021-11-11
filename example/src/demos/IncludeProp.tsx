20580430fef5c972cddc9d086b14ab5b3561e34f
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-11 06:52:43 -0600

    chore: cleanup example with styled-components

--- example/src/demos/IncludeProp.tsx
+++ example/src/demos/IncludeProp.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useThree, Canvas } from '@react-three/fiber'
+import styled from 'styled-components'
 
 function Observer({ setFrameloop }: any) {
   const frameloop = useThree((state) => state.frameloop)
@@ -13,36 +14,43 @@ export default function Demo() {
   const [frameloop, setFrameloop] = useState('never')
 
   return (
-    <div
-      style={{
-        background: frameloop === 'never' ? 'white' : 'rgba(0, 0, 255, 0.08)',
-        height: '100vh',
-        overflowY: 'scroll',
-      }}>
-      <p
-        style={{
-          position: 'fixed',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          fontSize: '48px',
-          color: 'black',
-          textAlign: 'center',
-        }}>
+    <DemoContainer frameloop={frameloop}>
+      <DemoText>
         Scroll ↓
         <br />
-        <span style={{ fontSize: '24px', color: 'black', opacity: 0.7 }}>Frameloop: {frameloop}</span>
-      </p>
-      <div
-        style={{
-          height: '220vh',
-          paddingBottom: '120vh',
-        }}>
+        <DemoDetails>Frameloop: {frameloop}</DemoDetails>
+      </DemoText>
+      <div style={{ height: '220vh', paddingBottom: '120vh' }}>
         <Canvas intersect camera={{ position: [0, 1.3, 3] }}>
           <Observer setFrameloop={setFrameloop} />
           <gridHelper args={[2, 2]} />
         </Canvas>
       </div>
-    </div>
+    </DemoContainer>
   )
 }
+
+interface DemoContainerProps {
+  frameloop: string
+}
+
+const DemoContainer = styled.main<DemoContainerProps>`
+  background: ${(props) => (props.frameloop === 'never' ? 'white' : 'rgba(0, 0, 255, 0.08)')};
+  height: 100vh;
+  overflow-y: scroll;
+`
+
+const DemoText = styled.p`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 48px;
+  color: black;
+  text-align: center;
+`
+
+const DemoDetails = styled.span`
+  font-size: 24px;
+  color: rgba(0, 0, 0, 0.7);
+`
