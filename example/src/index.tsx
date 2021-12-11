8e45128eb3dd299bf6c02ed9e7bd21ff37b9eede
— <drcmda@gmail.com>
2021-12-11 11:40:25 +0100

    move up to react rc

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
-/*import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-*/
-
-import { render, extend } from '@react-three/fiber'
-import { Mesh } from 'three'
-
-render(<mesh />, document.getElementById('root'))
