a6715a7a173a795c1b3c7a8b3ff80849afa5d971
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-12-12 04:55:46 -0600

    chore: bump RTR to rc

--- packages/fiber/tests/native/canvas.test.tsx
+++ packages/fiber/tests/native/canvas.test.tsx
@@ -1,29 +1,29 @@
 import * as React from 'react'
 import { View } from 'react-native'
-import { render, RenderAPI } from '@testing-library/react-native'
+import { act, create, ReactTestRenderer } from 'react-test-renderer'
 
-import { Canvas, act } from '../../src/native'
+import { Canvas } from '../../src/native'
 
 describe('native Canvas', () => {
   it('should correctly mount', async () => {
-    let renderer: RenderAPI = null!
+    let renderer: ReactTestRenderer = null!
 
     await act(async () => {
-      renderer = render(
+      renderer = create(
         <Canvas>
           <group />
         </Canvas>,
       )
     })
 
-    expect(renderer.container).toMatchSnapshot()
+    expect(renderer.toTree()).toMatchSnapshot()
   })
 
   it('should forward ref', async () => {
     const ref = React.createRef<View>()
 
     await act(async () => {
-      render(
+      create(
         <Canvas ref={ref}>
           <group />
         </Canvas>,
@@ -34,10 +34,10 @@ describe('native Canvas', () => {
   })
 
   it('should correctly unmount', async () => {
-    let renderer: RenderAPI = null!
+    let renderer: ReactTestRenderer = null!
 
     await act(async () => {
-      renderer = render(
+      renderer = create(
         <Canvas>
           <group />
         </Canvas>,
