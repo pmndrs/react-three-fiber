a88a1316b50a056ce4285285be7be1a4538630f5
Jamie Rolfs <jrolfs@users.noreply.github.com>
2022-02-10 19:33:43 -0800

    fix: export fiber internal types for RTTR (#2066)
    * fix(fiber): export `BaseInstance` and `LocalState` types from renderer
    
    * fix(test-renderer): import types correctly from fiber package

--- packages/fiber/src/index.tsx
+++ packages/fiber/src/index.tsx
@@ -2,6 +2,7 @@
 export * from './three-types'
 import * as ReactThreeFiber from './three-types'
 export { ReactThreeFiber }
+export type { BaseInstance, LocalState } from './core/renderer'
 export type {
   Intersection,
   Subscription,
