fff49b05d640be9a6e6726366a03479fad0889fd
— <drcmda@gmail.com>
2022-03-08 20:59:06 +0100

    rc

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+// @ts-expect-error
+import * as ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
