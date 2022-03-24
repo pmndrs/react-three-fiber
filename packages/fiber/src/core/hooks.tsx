276c9dfa5de09765d744b81c7061544e98ad8527
susiwen8 <susiwen8@gmail.com>
2022-03-24 16:47:20 +0800

    fix(event): memoize `onPointerMissed`

--- packages/fiber/src/core/hooks.tsx
+++ packages/fiber/src/core/hooks.tsx
@@ -19,6 +19,8 @@ export type Extensions = (loader: THREE.Loader) => void
 export type LoaderResult<T> = T extends any[] ? Loader<T[number]> : Loader<T>
 export type ConditionalType<Child, Parent, Truthy, Falsy> = Child extends Parent ? Truthy : Falsy
 export type BranchingReturn<T, Parent, Coerced> = ConditionalType<T, Parent, Coerced, T>
+type noop = (this: any, ...args: any[]) => any
+type PickFunction<T extends noop> = (this: ThisParameterType<T>, ...args: Parameters<T>) => ReturnType<T>
 
 export function useStore() {
   const store = React.useContext(context)
@@ -104,6 +106,19 @@ function loadingFn<T>(extensions?: Extensions, onProgress?: (event: ProgressEven
       ),
     )
   }
+}
+
+export function useMemoizedFn<T extends noop>(fn: T) {
+  const fnRef = React.useRef<T>(fn)
+  fnRef.current = React.useMemo(() => fn, [fn])
+
+  const memoizedFn = React.useRef<PickFunction<T>>()
+  if (!memoizedFn.current) {
+    memoizedFn.current = function (this, ...args) {
+      return fnRef.current.apply(this, args)
+    }
+  }
+  return memoizedFn.current
 }
 
 export function useLoader<T, U extends string | string[]>(
