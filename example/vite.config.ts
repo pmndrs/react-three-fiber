66031ffc4e59e2d13eb404d63366f24549ff59a9
Gianmarco <gianmarcosimone89@gmail.com>
2022-03-25 14:11:10 +0100

    .

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,11 +3,5 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      react: path.resolve('../node_modules/react'),
-      'react-dom': path.resolve('../node_modules/react-dom'),
-    },
-  },
   plugins: [reactRefresh()],
 })
