eabc13ba7d0c08a69b98a936830cd0bc6118d353
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-08-20 15:05:08 -0500

    fix: revert commit on finalizeInitialChildren

--- packages/fiber/src/core/renderer.ts
+++ packages/fiber/src/core/renderer.ts
@@ -328,11 +328,8 @@ function createRenderer<TCanvas>(_roots: Map<TCanvas, Root>, _getEventPriority?:
       Object.assign(instance.props, newProps)
       applyProps(instance.object, changedProps)
     },
-    finalizeInitialChildren(instance) {
-      commitInstance(instance)
-      return false
-    },
-    // commitMount: commitInstance,
+    finalizeInitialChildren: () => true,
+    commitMount: commitInstance,
     getPublicInstance: (instance) => instance?.object,
     prepareForCommit: () => null,
     preparePortalMount: (container) => container,
