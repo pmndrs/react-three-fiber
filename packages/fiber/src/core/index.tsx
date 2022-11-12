bf66f085290a99a35191e4c71c1f97d5c1239e70
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-11-12 12:30:48 -0600

    Revert "fix(core): set THREE.ColorManagement.enabled if able"
    This reverts commit aa87d3e6616053189fcf787444fb899cf596588c.

--- packages/fiber/src/core/index.tsx
+++ packages/fiber/src/core/index.tsx
@@ -28,7 +28,7 @@ import {
   useIsomorphicLayoutEffect,
   Camera,
   updateCamera,
-  ColorManagement,
+  setDeep,
 } from './utils'
 import { useStore } from './hooks'
 import { OffscreenCanvas } from 'three'
@@ -273,9 +273,8 @@ function createRoot<TCanvas extends Element>(canvas: TCanvas): ReconcilerRoot<TC
 
       // Safely set color management if available.
       // Avoid accessing THREE.ColorManagement to play nice with older versions
-      if (ColorManagement) {
-        if ('enabled' in ColorManagement) ColorManagement.enabled = legacy
-        else if ('legacyMode' in ColorManagement) ColorManagement.legacyMode = legacy
+      if ('ColorManagement' in THREE) {
+        setDeep(THREE, legacy, ['ColorManagement', 'legacyMode'])
       }
       const outputEncoding = linear ? THREE.LinearEncoding : THREE.sRGBEncoding
       const toneMapping = flat ? THREE.NoToneMapping : THREE.ACESFilmicToneMapping
