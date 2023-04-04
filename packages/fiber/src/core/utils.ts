2c8acf6d94a641b658eb2c0aed74312894d169b1
Cody Bennett <hi@codyb.co>
2023-04-04 05:18:32 -0500

    chore: fix types

--- packages/fiber/src/core/utils.ts
+++ packages/fiber/src/core/utils.ts
@@ -364,10 +364,10 @@ export function applyProps(instance: Instance, data: InstanceProps | DiffSet) {
         currentInstance[key].format === THREE.RGBAFormat &&
         currentInstance[key].type === THREE.UnsignedByteType
       ) {
-        const texture = currentInstance[key] as THREE.Texture
-        if ('colorSpace' in texture && 'outputColorSpace' in rootState.gl)
-          texture.colorSpace = rootState.gl.outputColorSpace
-        else texture.encoding = rootState.gl.outputEncoding
+        const texture = currentInstance[key] as THREE.Texture & { colorSpace?: string }
+        const gl = rootState.gl as THREE.WebGLRenderer & { outputColorSpace?: string }
+        if ('colorSpace' in texture) texture.colorSpace = gl.outputColorSpace
+        else texture.encoding = gl.outputEncoding
       }
     }
 
