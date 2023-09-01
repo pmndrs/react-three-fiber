3c91eac2f7c1fcbcb7cb287a5584d59585bd0dd3
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-08-31 19:41:26 -0500

    fix: offline data to fs for JSI OOM

--- packages/fiber/src/native/polyfills.ts
+++ packages/fiber/src/native/polyfills.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { Platform, NativeModules, Image } from 'react-native'
-import type { Asset } from 'expo-asset'
+import { Asset } from 'expo-asset'
+import * as fs from 'expo-file-system'
 
 if (Platform.OS !== 'web') {
   const BlobManager = require('react-native/Libraries/Blob/BlobManager.js')
@@ -73,20 +74,21 @@ if (Platform.OS !== 'web') {
     return `${BLOB_URL_PREFIX}${blob.data.blobId}?offset=${blob.data.offset}&size=${blob.size}`
   }
 
-  // Check if expo-asset is installed (available with expo modules)
-  let expAsset: typeof Asset | undefined
-  try {
-    expAsset = require('expo-asset')?.Asset
-  } catch (_) {}
-
   /**
    * Generates an asset based on input type.
    */
   async function getAsset(input: string | number): Promise<Asset> {
     switch (typeof input) {
       case 'string':
-        if (input.startsWith('data:')) return { localUri: input } as Asset
-        if (input.startsWith('blob:')) {
+        if (input.startsWith('data:')) {
+          const [header, data] = input.split(',')
+          const [, type] = header.split('/')
+
+          const localUri = fs.cacheDirectory + uuidv4() + `.${type}`
+          await fs.writeAsStringAsync(localUri, data, { encoding: fs.EncodingType.Base64 })
+
+          return { localUri } as Asset
+        } else if (input.startsWith('blob:')) {
           const blob = await new Promise<Blob>((res, rej) => {
             const xhr = new XMLHttpRequest()
             xhr.open('GET', input)
@@ -107,123 +109,121 @@ if (Platform.OS !== 'web') {
 
           return getAsset(localUri)
         }
-        return expAsset!.fromURI(input).downloadAsync()
+        return Asset.fromURI(input).downloadAsync()
       case 'number':
-        return expAsset!.fromModule(input).downloadAsync()
+        return Asset.fromModule(input).downloadAsync()
       default:
         throw new Error('R3F: Invalid asset! Must be a URI or module.')
     }
   }
 
-  if (expAsset) {
-    // Don't pre-process urls, let expo-asset generate an absolute URL
-    const extractUrlBase = THREE.LoaderUtils.extractUrlBase.bind(THREE.LoaderUtils)
-    THREE.LoaderUtils.extractUrlBase = (url: string) => (typeof url === 'string' ? extractUrlBase(url) : './')
+  // Don't pre-process urls, let expo-asset generate an absolute URL
+  const extractUrlBase = THREE.LoaderUtils.extractUrlBase.bind(THREE.LoaderUtils)
+  THREE.LoaderUtils.extractUrlBase = (url: string) => (typeof url === 'string' ? extractUrlBase(url) : './')
 
-    // There's no Image in native, so create a data texture instead
-    THREE.TextureLoader.prototype.load = function load(url, onLoad, onProgress, onError) {
-      const texture = new THREE.Texture()
+  // There's no Image in native, so create a data texture instead
+  THREE.TextureLoader.prototype.load = function load(url, onLoad, onProgress, onError) {
+    const texture = new THREE.Texture()
 
-      getAsset(url)
-        .then(async (asset: Asset) => {
-          if (!asset.width || !asset.height) {
-            const { width, height } = await new Promise<{ width: number; height: number }>((res, rej) =>
-              Image.getSize(asset.localUri!, (width, height) => res({ width, height }), rej),
-            )
-            asset.width = width
-            asset.height = height
-          }
-
-          texture.image = {
-            data: { localUri: asset.localUri },
-            width: asset.width,
-            height: asset.height,
-          }
-          texture.flipY = true
-          // texture.unpackAlignment = 1
-          texture.needsUpdate = true
-
-          // @ts-ignore
-          texture.isDataTexture = true
-
-          onLoad?.(texture)
-        })
-        .catch(onError)
-
-      return texture
-    }
-
-    // Fetches assets via XMLHttpRequest
-    THREE.FileLoader.prototype.load = function load(url, onLoad, onProgress, onError) {
-      if (this.path) url = this.path + url
-
-      const request = new XMLHttpRequest()
-
-      getAsset(url)
-        .then((asset) => {
-          request.open('GET', asset.uri, true)
-
-          request.addEventListener(
-            'load',
-            (event) => {
-              if (request.status === 200) {
-                onLoad?.(request.response)
-
-                this.manager.itemEnd(url)
-              } else {
-                onError?.(event as unknown as ErrorEvent)
-
-                this.manager.itemError(url)
-                this.manager.itemEnd(url)
-              }
-            },
-            false,
+    getAsset(url)
+      .then(async (asset: Asset) => {
+        if (!asset.width || !asset.height) {
+          const { width, height } = await new Promise<{ width: number; height: number }>((res, rej) =>
+            Image.getSize(asset.localUri!, (width, height) => res({ width, height }), rej),
           )
+          asset.width = width
+          asset.height = height
+        }
 
-          request.addEventListener(
-            'progress',
-            (event) => {
-              onProgress?.(event)
-            },
-            false,
-          )
+        texture.image = {
+          data: { localUri: asset.localUri },
+          width: asset.width,
+          height: asset.height,
+        }
+        texture.flipY = true
+        // texture.unpackAlignment = 1
+        texture.needsUpdate = true
 
-          request.addEventListener(
-            'error',
-            (event) => {
+        // @ts-ignore
+        texture.isDataTexture = true
+
+        onLoad?.(texture)
+      })
+      .catch(onError)
+
+    return texture
+  }
+
+  // Fetches assets via XMLHttpRequest
+  THREE.FileLoader.prototype.load = function load(url, onLoad, onProgress, onError) {
+    if (this.path) url = this.path + url
+
+    const request = new XMLHttpRequest()
+
+    getAsset(url)
+      .then((asset) => {
+        request.open('GET', asset.uri, true)
+
+        request.addEventListener(
+          'load',
+          (event) => {
+            if (request.status === 200) {
+              onLoad?.(request.response)
+
+              this.manager.itemEnd(url)
+            } else {
               onError?.(event as unknown as ErrorEvent)
 
               this.manager.itemError(url)
               this.manager.itemEnd(url)
-            },
-            false,
-          )
+            }
+          },
+          false,
+        )
 
-          request.addEventListener(
-            'abort',
-            (event) => {
-              onError?.(event as unknown as ErrorEvent)
+        request.addEventListener(
+          'progress',
+          (event) => {
+            onProgress?.(event)
+          },
+          false,
+        )
 
-              this.manager.itemError(url)
-              this.manager.itemEnd(url)
-            },
-            false,
-          )
+        request.addEventListener(
+          'error',
+          (event) => {
+            onError?.(event as unknown as ErrorEvent)
 
-          if (this.responseType) request.responseType = this.responseType
-          if (this.withCredentials) request.withCredentials = this.withCredentials
+            this.manager.itemError(url)
+            this.manager.itemEnd(url)
+          },
+          false,
+        )
 
-          for (const header in this.requestHeader) {
-            request.setRequestHeader(header, this.requestHeader[header])
-          }
+        request.addEventListener(
+          'abort',
+          (event) => {
+            onError?.(event as unknown as ErrorEvent)
 
-          request.send(null)
+            this.manager.itemError(url)
+            this.manager.itemEnd(url)
+          },
+          false,
+        )
 
-          this.manager.itemStart(url)
-        })
-        .catch(onError)
+        if (this.responseType) request.responseType = this.responseType
+        if (this.withCredentials) request.withCredentials = this.withCredentials
 
-      return request
-    }
+        for (const header in this.requestHeader) {
+          request.setRequestHeader(header, this.requestHeader[header])
+        }
+
+        request.send(null)
+
+        this.manager.itemStart(url)
+      })
+      .catch(onError)
+
+    return request
   }
 }
