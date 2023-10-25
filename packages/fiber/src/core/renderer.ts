29372557ea917a5e579146a02a7a868a08b73a8a
Cody Bennett <hi@codyb.co>
2023-10-25 05:22:35 -0500

    fix: insertBefore on swap

--- packages/fiber/src/core/renderer.ts
+++ packages/fiber/src/core/renderer.ts
@@ -31,7 +31,6 @@ export type LocalState = {
   attach?: AttachType
   previousAttach: any
   memoizedProps: { [key: string]: any }
-  autoRemovedBeforeAppend?: boolean
 }
 
 export type AttachFnType = (parent: Instance, self: Instance) => () => void
@@ -272,13 +271,8 @@ function createRenderer<TCanvas>(_roots: Map<TCanvas, Root>, _getEventPriority?:
     instance.__r3f.objects.forEach((child) => appendChild(newInstance, child))
     instance.__r3f.objects = []
 
-    if (!instance.__r3f.autoRemovedBeforeAppend) {
-      removeChild(parent, instance)
-    }
-    if (newInstance.parent) {
-      newInstance.__r3f.autoRemovedBeforeAppend = true
-    }
-    appendChild(parent, newInstance)
+    insertBefore(parent, newInstance, instance)
+    removeChild(parent, instance)
 
     // Re-bind event handlers
     if (newInstance.raycast && newInstance.__r3f.eventCount) {
