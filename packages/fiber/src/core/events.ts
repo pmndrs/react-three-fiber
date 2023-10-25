60ac7d7344c443d348371a2d38edaf51af478a4e
Cody Bennett <hi@codyb.co>
2023-10-25 04:21:34 -0500

    fix(types): remove use of Object3D generic

--- packages/fiber/src/core/events.ts
+++ packages/fiber/src/core/events.ts
@@ -205,7 +205,7 @@ export function createEvents(store: UseBoundStore<RootState>) {
       state.events.compute?.(event, state)
     }
 
-    function handleRaycast(obj: THREE.Object3D<THREE.Event>) {
+    function handleRaycast(obj: THREE.Object3D) {
       const state = getRootState(obj)
       // Skip event handling when noEvents is set, or when the raycasters camera is null
       if (!state || !state.events.enabled || state.raycaster.camera === null) return []
@@ -222,7 +222,7 @@ export function createEvents(store: UseBoundStore<RootState>) {
     }
 
     // Collect events
-    let hits: THREE.Intersection<THREE.Object3D<THREE.Event>>[] = eventsObjects
+    let hits: THREE.Intersection<THREE.Object3D>[] = eventsObjects
       // Intersect objects
       .flatMap(handleRaycast)
       // Sort by event priority and distance
