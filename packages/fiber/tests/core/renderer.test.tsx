d183fa5fb08130a1fd966bb2ccaaf8d49dc45b7b
Cody Bennett <hi@codyb.co>
2023-10-26 02:20:46 -0500

    chore(tests): add array attach case

--- packages/fiber/tests/core/renderer.test.tsx
+++ packages/fiber/tests/core/renderer.test.tsx
@@ -546,6 +546,57 @@ describe('renderer', () => {
     expect(state.scene.children[3]).toBe(c)
   })
 
+  it('can swap 4 array primitives via attach', async () => {
+    let state: RootState = null!
+    const a = new THREE.Group()
+    const b = new THREE.Group()
+    const c = new THREE.Group()
+    const d = new THREE.Group()
+    const array = [a, b, c, d]
+
+    const Test = ({ array }: { array: THREE.Group[] }) => (
+      <>
+        {array.map((group, i) => (
+          <primitive key={i} attach={`userData-objects-${i}`} object={group} />
+        ))}
+      </>
+    )
+
+    await act(async () => {
+      state = root.render(<Test array={array} />).getState()
+    })
+
+    expect(state.scene.children.length).toBe(0)
+    expect(state.scene.userData.objects[0]).toBe(a)
+    expect(state.scene.userData.objects[1]).toBe(b)
+    expect(state.scene.userData.objects[2]).toBe(c)
+    expect(state.scene.userData.objects[3]).toBe(d)
+
+    const reversedArray = [...array.reverse()]
+
+    await act(async () => {
+      state = root.render(<Test array={reversedArray} />).getState()
+    })
+
+    expect(state.scene.children.length).toBe(0)
+    expect(state.scene.userData.objects[0]).toBe(d)
+    expect(state.scene.userData.objects[1]).toBe(c)
+    expect(state.scene.userData.objects[2]).toBe(b)
+    expect(state.scene.userData.objects[3]).toBe(a)
+
+    const mixedArray = [b, a, d, c]
+
+    await act(async () => {
+      state = root.render(<Test array={mixedArray} />).getState()
+    })
+
+    expect(state.scene.children.length).toBe(0)
+    expect(state.scene.userData.objects[0]).toBe(b)
+    expect(state.scene.userData.objects[1]).toBe(a)
+    expect(state.scene.userData.objects[2]).toBe(d)
+    expect(state.scene.userData.objects[3]).toBe(c)
+  })
+
   it('will make an Orthographic Camera & set the position', async () => {
     let camera: THREE.Camera = null!
 
