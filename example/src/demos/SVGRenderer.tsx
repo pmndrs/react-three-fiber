b252c8a55aa648a43427a6442aa77660c10e9e54
Cody Bennett <hi@codyb.co>
2024-04-27 19:20:57 -0500

    chore(demos): cleanup SVGRenderer

--- example/src/demos/SVGRenderer.tsx
+++ example/src/demos/SVGRenderer.tsx
@@ -1,16 +1,6 @@
 import * as THREE from 'three'
-import React, { useRef, useState } from 'react'
-import {
-  RenderProps,
-  extend,
-  createRoot,
-  unmountComponentAtNode,
-  useFrame,
-  events,
-  ReconcilerRoot,
-} from '@react-three/fiber'
-import useMeasure, { Options as ResizeOptions } from 'react-use-measure'
-import mergeRefs from 'react-merge-refs'
+import React, { useEffect, useRef, useState } from 'react'
+import { useFrame, Canvas } from '@react-three/fiber'
 import { SVGRenderer } from 'three-stdlib'
 
 function TorusKnot() {
@@ -28,48 +18,21 @@ function TorusKnot() {
   )
 }
 
+const gl = new SVGRenderer()
+gl.domElement.style.position = 'absolute'
+gl.domElement.style.top = 0
+gl.domElement.style.left = 0
+
 export default function () {
-  return (
-    <Canvas camera={{ position: [0, 0, 50] }}>
-      <color attach="background" args={['#dedddf']} />
-      <TorusKnot />
-    </Canvas>
-  )
-}
-
-interface Props extends Omit<RenderProps<HTMLCanvasElement>, 'size' | 'gl'>, React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode
-  resize?: ResizeOptions
-}
-
-function Canvas({ children, resize, style, className, ...props }: Props) {
-  React.useMemo(() => extend(THREE), [])
-
-  const [bind, size] = useMeasure({ scroll: true, debounce: { scroll: 50, resize: 0 }, ...resize })
-  const ref = React.useRef<HTMLDivElement>(null!)
-  const [gl] = useState(() => new SVGRenderer() as unknown as THREE.WebGLRenderer)
-  const root = React.useRef<ReconcilerRoot<HTMLElement>>(null!)
-
-  if (size.width > 0 && size.height > 0) {
-    if (!root.current) root.current = createRoot<HTMLElement>(ref.current)
-    root.current.configure({ ...props, size, events, gl })
-    root.current.render(children)
-  }
-
-  React.useEffect(() => {
-    const container = ref.current
-    container.appendChild(gl.domElement)
-    return () => {
-      container.removeChild(gl.domElement)
-      unmountComponentAtNode(container)
-    }
+  useEffect(() => {
+    document.body.appendChild(gl.domElement)
+    return () => document.body.removeChild(gl.domElement)
   }, [])
 
   return (
-    <div
-      ref={mergeRefs([ref, bind])}
-      className={className}
-      style={{ position: 'relative', width: '100%', height: '100%', overflow: 'hidden', ...style }}
-    />
+    <Canvas gl={gl} camera={{ position: [0, 0, 50] }} eventSource={gl.domElement}>
+      <color attach="background" args={['#dedddf']} />
+      <TorusKnot />
+    </Canvas>
   )
 }
