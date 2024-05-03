cc3080b214c297a6d15c57d4de7c14f398cc721b
Cody Bennett <hi@codyb.co>
2024-05-03 11:44:45 -0500

    chore(tests): fix typo in sorted attach case

--- packages/fiber/tests/renderer.test.tsx
+++ packages/fiber/tests/renderer.test.tsx
@@ -456,7 +456,7 @@ describe('renderer', () => {
     const c = new THREE.Group()
     c.name = 'c'
     const d = new THREE.Group()
-    d.name = 'c'
+    d.name = 'd'
     const array = [a, b, c, d]
 
     const Test = ({ array }: { array: THREE.Group[] }) => (
