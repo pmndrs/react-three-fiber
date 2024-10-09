37fdbec89543ca01ff93a0771c88c706f6d571d3
Bela Bohlender <85560643+bbohlender@users.noreply.github.com>
2024-10-09 02:38:58 +0200

    fix: ThreeEvent should not include initMouseEvent (#3372)

--- packages/fiber/src/three-types.ts
+++ packages/fiber/src/three-types.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { EventHandlers } from './core/events'
 import { AttachType } from './core/renderer'
 
-export type Properties<T> = Pick<T, { [K in keyof T]: T[K] extends (_: any) => any ? never : K }[keyof T]>
+export type Properties<T> = { [K in keyof T as T[K] extends (...args: Array<any>) => any ? never : K]: T[K] }
 export type NonFunctionKeys<T> = { [K in keyof T]-?: T[K] extends Function ? never : K }[keyof T]
 export type Overwrite<T, O> = Omit<T, NonFunctionKeys<O>> & O
 
