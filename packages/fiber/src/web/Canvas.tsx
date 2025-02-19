98996eeee1e716780c7ea6d5c845709437e089ba
Joevin <104806938+joevingracien@users.noreply.github.com>
2025-02-19 13:56:20 +0100

    feat: make children optional in Canvas (#3264)

--- packages/fiber/src/web/Canvas.tsx
+++ packages/fiber/src/web/Canvas.tsx
@@ -10,7 +10,7 @@ import { DomEvent } from '../core/events'
 export interface CanvasProps
   extends Omit<RenderProps<HTMLCanvasElement>, 'size'>,
     React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode
+  children?: React.ReactNode
   /** Canvas fallback content, similar to img's alt prop */
   fallback?: React.ReactNode
   /**
@@ -120,7 +120,7 @@ const CanvasImpl = /*#__PURE__*/ React.forwardRef<HTMLCanvasElement, Props>(func
       root.current.render(
         <Bridge>
           <ErrorBoundary set={setError}>
-            <React.Suspense fallback={<Block set={setBlock} />}>{children}</React.Suspense>
+            <React.Suspense fallback={<Block set={setBlock} />}>{children ?? null}</React.Suspense>
           </ErrorBoundary>
         </Bridge>,
       )
