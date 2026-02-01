69b4ede4965dff646e1c4fa73464a456a5bdc05a
Dennis Smolek <desopolis@gmail.com>
2026-02-01 21:25:40 +0900

    chore: remove heroes

--- heroes/src/demos/Splash/specialNodes/utils.ts
+++
@@ -1,6 +0,0 @@
-// Fn Node to take the position Local and bounds and return a objectSpace Position
-import { positionWorld } from 'three/tsl'
-export function objectSpacePosition(boundsMin: any, boundsMax: any) {
-  // bounds come from setFromObject (world-space), so use positionWorld to match
-  return positionWorld.sub(boundsMin).div(boundsMax.sub(boundsMin))
-}
